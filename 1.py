b3edb2e14f99d008bc289d5821a7eb06629e2cf8
BBarel <bbarel80@gmail.com>
2020-12-20 09:45:57 +0200

    homework YEET

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
 uname = str(input("Enter your username "))
 shtrodel = ("@" in uname)
 dot = ("." in uname)
+
 if (dot == 1) and (shtrodel >= 1):
     pword = str(input("Enter your password "))
     solamit = ("#" in pword)
