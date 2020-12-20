9cab10f0219fda67405bfcbe2f6c7f16c0366544
BBarel <bbarel80@gmail.com>
2020-12-20 09:44:37 +0200

    homework YEET

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
-uname = str(input("enter your username "))
+uname = str(input("Enter your username "))
 shtrodel = ("@" in uname)
 dot = ("." in uname)
-if (dot == 1) and (shtrodel == 1):
-    pword = str(input("enter your password "))
+if (dot == 1) and (shtrodel >= 1):
+    pword = str(input("Enter your password "))
     solamit = ("#" in pword)
     pword_len = len(pword)
     if solamit == 1 and pword_len >= 8:
-        print("yeahhhhh boiiiii")
+        print("Yeahhhhh Boiiiii")
     else:
         print("Invalid Password")
 else:
